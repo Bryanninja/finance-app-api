865203b56a63ba0e1e0ebd43b3ca965e6f0ea1b5
bryan <bryannascimentopl800@gmail.com>
2026-08-18 11:32:23 -0300

    feat: add PostgresGetUserByEmailRepository

---
+++ src/use-cases/create-user.js
@@ -0,0 +1,36 @@
+import { v4 as uuidv4 } from 'uuid';
+import bcrypt from 'bcrypt';
+import { PostgresCreateUserRepository } from '../repositories/postgres/create-user.js';
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
+
+export class CreateUserUseCase {
+  async execute(createUserParams) {
+    //TODO: verificar se o e-mail já está em uso
+    const postgresGetUserByEmailRepository =
+      new PostgresGetUserByEmailRepository();
+    const userWithProvidedEmail =
+      await postgresGetUserByEmailRepository.execute(createUserParams.email);
+
+    if (userWithProvidedEmail) {
+      throw new Error('The Provided Email is alredy in use.');
+    }
+
+    //gerar ID do usuario
+    const userId = uuidv4();
+
+    // criptografar a senha
+    const hashedPassword = await bcrypt.hash(createUserParams.password, 10);
+
+    //inserir o usúario no banco de dados
+    const user = {
+      ...createUserParams,
+      id: userId,
+      password: hashedPassword,
+    };
+
+    //chamar o repositorio
+    const postgresCreateUserRepository = new PostgresCreateUserRepository();
+    const createdUser = await postgresCreateUserRepository.execute(user);
+    return createdUser;
+  }
+}
