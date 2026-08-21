236a0402faf4440dc269b34a3ebfdabff4de338c
bryan <bryannascimentopl800@gmail.com>
2026-08-21 12:47:39 -0300

    feat: add index.js file to use-cases and repositories

--- src/controllers/update-user.js
+++ src/controllers/update-user.js
@@ -1,5 +1,5 @@
 import { EmailAlredyInUseError } from '../errors/users.js';
-import { UpdateUserUseCase } from '../use-cases/update-user.js';
+import { UpdateUserUseCase } from '../use-cases/index.js';
 
 import {
   checkIfEmailIsValid,
--- src/use-cases/update-user.js
+++ src/use-cases/update-user.js
@@ -1,7 +1,9 @@
 import { EmailAlredyInUseError } from '../errors/users.js';
-import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js';
+import {
+  PostgresGetUserByEmailRepository,
+  PostgresUpdateUserRepository,
+} from '../repositories/postgres/index.js';
 import bcrypt from 'bcrypt';
-import { PostgresUpdateUserRepository } from '../repositories/postgres/update-users.js';
 
 export class UpdateUserUseCase {
   async execute(userId, updateUserParams) {
