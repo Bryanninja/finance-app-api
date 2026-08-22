23c5da8b02e9bf5ee538615e396391bbe85d3a2b
bryan <bryannascimentopl800@gmail.com>
2026-08-22 15:08:46 -0300

    feat: add DeleteUserUseCase

---
+++ src/use-cases/delete-user.js
@@ -0,0 +1,10 @@
+import PostgresDeleteUserRepository from '../repositories/postgres/index.js';
+
+export class DeleteUserUseCase {
+  async execute(userId) {
+    const postgresDeleteUserRepository = new PostgresDeleteUserRepository();
+    const deleteUser = await postgresDeleteUserRepository.execute(userId);
+
+    return deleteUser;
+  }
+}
