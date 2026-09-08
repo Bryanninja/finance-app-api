65f8ffaa29143d1e21a82b8b0592bbbfa4f30af5
bryan <bryannascimentopl800@gmail.com>
2026-09-08 19:16:53 -0300

    feat: add GetUserBalanceUseCase

---
+++ src/use-cases/user/get-user-balance.js
@@ -0,0 +1,21 @@
+import { UserNotFoundError } from '../../errors/users.js';
+
+export class GetUserBalanceUseCase {
+  constructor(getUserBalanceRepository, getUserByIdRepository) {
+    this.getUserBalanceRepository = getUserBalanceRepository;
+    this.getUserByIdRepository = getUserByIdRepository;
+  }
+
+  async execute(params) {
+    const { userId } = params;
+
+    const userIsValid = await this.getUserByIdRepository(userId);
+    if (!userIsValid) {
+      throw new UserNotFoundError();
+    }
+
+    const balance = await this.getUserBalanceRepository.execute(userId);
+
+    return balance;
+  }
+}
